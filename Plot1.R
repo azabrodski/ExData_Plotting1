8612dcb18ec05ef9224b06b811c032226550758e
azabrodski <azabrodski@users.noreply.github.com>
2015-05-07 15:48:03 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -8,6 +8,7 @@ close(fi)
 dates <- paste(df$Date, df$Time, sep = " ")
 dates <- dmy_hms(dates)
 df$Date_Time <- dates
+
 png(filename = "Plot1.png", width = 480, height = 480)
 hist(df$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylim = c(0,1200), bg = "white")
 dev.off()
